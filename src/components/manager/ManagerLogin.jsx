52be8c8f22dab9c790a71b290c7320be836e53df
Tonnyvellor <tony@gmail.com>
2024-12-04 21:53:06 -0800

    manger route

--- src/components/manager/ManagerLogin.jsx
+++ src/components/manager/ManagerLogin.jsx
@@ -250,7 +250,7 @@ const ManagerLogin = () => {
       toast.success(data.message);
       localStorage.setItem('managerID', data.user.managerId); // Store manager ID
       sessionStorage.setItem('sessionId', data.sessionId); // Store session ID
-      navigate("/BrandIntro");
+      navigate("/ManagerHome");
 
     } else {
       toast.error(data.error);
