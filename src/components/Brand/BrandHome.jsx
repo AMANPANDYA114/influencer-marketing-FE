05486715c44ee093841bb5cbf9ca7d50b540d076
Tonnyvellor <tony@gmail.com>
2025-01-22 02:37:55 -0800

    added new code

--- src/components/Brand/BrandHome.jsx
+++ src/components/Brand/BrandHome.jsx
@@ -299,17 +299,19 @@ const  BrandHome= () => {
       <div className="ml-[5%] flex-1 overflow-y-auto p-4 mb-0">
         <BrandHeader page="brand home" />
 
-{/*       
-<h1 className="text-xs sm:text-2xl md:text-3xl lg:text-4xl font-extrabold text-center text-black mt-[50px] mb-[50px] cursor-pointer hover:text-gray-700 transition-all duration-300 ease-in-out transform hover:scale-105">
+{/* 
+<h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold text-center text-black mt-[50px] mb-[50px] cursor-pointer hover:text-gray-700 transition-all duration-300 ease-in-out transform hover:scale-105 ml-[10px] break-words">
   Start Searching Influencers from a Large Database
-  <span className="block text-xs sm:text-sm md:text-base lg:text-lg font-normal text-gray-600 mt-0">
+  <span className="block text-base sm:text-sm md:text-base lg:text-lg font-normal text-gray-600 mt-0 ml-[10px] break-words">
     Discover top influencers to elevate your brand's reach and impact.
   </span>
 </h1> */}
 
-<h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold text-center text-black mt-[50px] mb-[50px] cursor-pointer hover:text-gray-700 transition-all duration-300 ease-in-out transform hover:scale-105 ml-[10px] break-words">
+
+
+<h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold text-center text-black mt-[50px] mb-[50px] cursor-pointer hover:text-gray-700 transition-all duration-300 ease-in-out transform hover:scale-105 ml-[20px] sm:ml-0 break-words">
   Start Searching Influencers from a Large Database
-  <span className="block text-base sm:text-sm md:text-base lg:text-lg font-normal text-gray-600 mt-0 ml-[10px] break-words">
+  <span className="block text-base sm:text-sm md:text-base lg:text-lg font-normal text-gray-600 mt-0 ml-[20px] sm:ml-0 break-words">
     Discover top influencers to elevate your brand's reach and impact.
   </span>
 </h1>
@@ -464,9 +466,10 @@ const  BrandHome= () => {
         <CsvUploader  fetchData={fetchData}  /> 
 </div> */}
 
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-h-[calc(100vh-250px)] mt-[10%] mb-0">
+        {/* <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-h-[calc(100vh-250px)] mt-[10%] mb-0"> */}
 
- 
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-h-[calc(100vh-250px)] mt-[10%] mb-0 ml-[10px] sm:ml-0">
+
 <button
   onClick={() => setDrawerOpen(true)}
   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mb-2 flex items-center space-x-2 ml-[30px]"
